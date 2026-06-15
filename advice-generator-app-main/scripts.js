c4cc2315517f3fcfd96de20afcbe4bedf75befa6
Ayoola <Ayoola04web>
2026-06-15 17:02:39 +0100

    Completed Advice Generator App

---
+++ advice-generator-app-main/scripts.js
@@ -0,0 +1,26 @@
+
+const adviceNumber = document.getElementById("advice-number");
+const adviceText = document.getElementById("advice-text");
+const diceBtn = document.getElementById("dice-btn");
+
+async function getAdvice() {
+    try {
+      const response = await fetch("https://api.adviceslip.com/advice");
+
+      const data = await response.json();
+
+      adviceNumber.textContent = data.slip.id;
+      adviceText.textContent = `${data.slip.advice}`;
+
+    } catch (error) {
+        adviceText.textContent =
+           "Unable to load advice. Please try again.";
+
+    }
+    
+}
+
+getAdvice();
+
+diceBtn.addEventListener("click", getAdvice);
+    
